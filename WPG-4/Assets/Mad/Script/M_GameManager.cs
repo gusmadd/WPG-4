3a6e1ba075f6e8a673b5207de9a8b1639f57469f
gusmadd <afadkress@gmail.com>
2026-02-19 23:35:33 +0700

    .

--- WPG-4/Assets/Mad/Script/M_GameManager.cs
+++ WPG-4/Assets/Mad/Script/M_GameManager.cs
@@ -36,15 +36,7 @@ public class M_GameManager : MonoBehaviour
 
     void Awake()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+        Instance = this;
     }
 
     void Start()
